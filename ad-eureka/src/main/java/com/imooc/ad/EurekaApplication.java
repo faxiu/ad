168c9a97f4c734db97bab02a599e978246b9faaf
贺凯 <412097984@qq.com>
2019-07-03 16:17:50 +0800

    广告系统项目eureka和gateway功能的实现

---
+++ ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: hekai
+ * @Date: 2019-07-03 14:04
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
---
+++ ad-gateway/src/main/java/com/imooc/ad/ZuulGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author: hekai
+ * @Date: 2019-07-03 15:04
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
